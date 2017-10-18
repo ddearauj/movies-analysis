a7a99b236f2514db4f525a462a45cf16727d9307
Daniel Pereira <danielpereira03@gmail.com>
2017-10-18 12:52:34 -0200

    forgot to save the details table. Created functions to save only this table. Hope it does not take 14hrs again

--- getMoviesDetails.py
+++ getMoviesDetails.py
@@ -37,6 +37,12 @@ def get_data_from_payload(movie_data, details, prod_companies, prod_countries, g
 
 	return details, prod_companies, prod_countries, genres
 
+def get_details_from_payload(movie_data, details, movie_detais_headers, movie_id):
+	df = json_normalize(movie_data)
+	details = details.append(df[movie_detais_headers])
+
+	return details
+
 def get_details_from_ids(ids, conn, movie_detais_headers):
 
 	details = pd.DataFrame()
@@ -58,9 +64,30 @@ def get_details_from_ids(ids, conn, movie_detais_headers):
 				details, prod_companies, prod_countries, genres = get_data_from_payload(movie_data, details, prod_companies, prod_countries, genres, movie_detais_headers, movie_id)
 	print(genres.head(5))
 
+	details.to_csv('details.csv')
 	genres.to_csv('genres.csv')
 	prod_companies.to_csv("prod_companies.csv")
 	prod_countries.to_csv("prod_countries.csv")
+
+
+def get_details_only_from_ids(ids, conn, movie_detais_headers):
+
+	details = pd.DataFrame()
+	for movie_id in ids:
+		#get the data
+		url = get_url(movie_id, API_KEY)
+		movie_data = get_json(conn, url)
+		try:
+			details = get_details_from_payload(movie_data, details, movie_detais_headers, movie_id)
+		except Exception as e:
+			if (movie_data['status_code'] == 25):
+				#limit of 40 requests per 10 seconds reached!
+				time.sleep(10)
+				movie_data = get_json(conn, url)
+				details = get_details_from_payload(movie_data, details, movie_detais_headers, movie_id)
+
+	details.to_csv('details.csv')
+
 
 def main():
 	with open('out.csv', 'r') as f:
@@ -72,7 +99,8 @@ def main():
 	movie_detais_headers = ['budget', 'id', 'imdb_id', 'original_language', 'original_title', 'release_date', 'revenue', 'runtime', 'vote_average', 'vote_count']
 
 	conn = http.client.HTTPSConnection("api.themoviedb.org")
-	get_details_from_ids(ids, conn, movie_detais_headers)
+	#get_details_from_ids(ids, conn, movie_detais_headers)
+	get_details_only_from_ids(ids, conn, movie_detais_headers)
 
 
 
